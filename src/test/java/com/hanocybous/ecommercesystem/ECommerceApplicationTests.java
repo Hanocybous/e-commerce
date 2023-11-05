b807f5ba9969d38fd9bb9121b07957815bdb7657
Charilaos Chatzidimitriou <97763180+Hanocybous@users.noreply.github.com>
2023-11-05 23:07:16 +0200

    Update ECommerceApplicationTests.java
    Signed-off-by: Charilaos Chatzidimitriou <97763180+Hanocybous@users.noreply.github.com>

--- src/test/java/com/hanocybous/ecommercesystem/ECommerceApplicationTests.java
+++ src/test/java/com/hanocybous/ecommercesystem/ECommerceApplicationTests.java
@@ -2,12 +2,14 @@ package com.hanocybous.ecommercesystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertTrue;
 
 @SpringBootTest
 class ECommerceApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(1 == 1);
     }
 
 }
