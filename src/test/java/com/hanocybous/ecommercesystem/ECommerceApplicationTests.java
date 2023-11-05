be82333ed82a6202cac9771cd70b8dba8fb84393
Hanocybous <cs05387@uoi.gr>
2023-11-05 23:26:02 +0200

    Build Initial

--- src/test/java/com/hanocybous/ecommercesystem/ECommerceApplicationTests.java
+++ src/test/java/com/hanocybous/ecommercesystem/ECommerceApplicationTests.java
@@ -2,14 +2,14 @@ package com.hanocybous.ecommercesystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ECommerceApplicationTests {
 
     @Test
     void contextLoads() {
-        assertTrue(1 == 1);
+        assertEquals(1, 1);
     }
 
 }
